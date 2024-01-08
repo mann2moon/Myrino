f9a937207a88ebee8354109bb68fb12197db9380
amirali irvany <dev.metect@gmail.com>
2024-01-08 12:24:29 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name = 'myrino',
-    version = '0.1',
+    name='mypackage',
+    version='0.1',
     author='amirali irvany',
-    author_email = 'dev.metect@gmail.com',
-    description = 'Myrino is an api-based library for Rubino messengers',
-    keywords = ['Bot', 'rubino', 'rubika', 'myrino'],
-    long_description = open('README.md', encoding='utf-8').read(),
-    python_requires='~=3.6',
-    url = 'https://github.com/metect',
-    install_requires = ['requests'],
-    classifiers = [
-    	'Programming Language :: Python :: 3',
+    author_email='dev.metect@gmail.com',
+    description='Myrino is an api-based library for Rubino messengers',
+    url='https://github.com/metect',
+    install_requires=['requests'],
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
-    ]
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
