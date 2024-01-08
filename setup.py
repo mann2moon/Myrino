7293dcd8bcd662b6a554c56e7cf5cfb26d89fc96
amirali irvany <dev.metect@gmail.com>
2024-01-08 12:26:00 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mypackage',
+    name='myrino',
     version='0.1',
     author='amirali irvany',
     author_email='dev.metect@gmail.com',
